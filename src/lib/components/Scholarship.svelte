887b15cb74bcbb1391e2e968c454c444edebd2e2
apricitine <apricitinecas@gmail.com>
2026-02-26 16:36:09 -0800

    asdf

--- src/lib/components/Scholarship.svelte
+++ src/lib/components/Scholarship.svelte
@@ -1,8 +1,48 @@
+<script lang="ts">
+  interface Props {
+    name: string
+    deadline: string
+    description: string
+  }
+
+  let props: Props = $props()
+</script>
+
 <section>
+  <div id="name">{props.name}</div>
+  <div id="deadline">{props.deadline}</div>
+  <div id="description">{props.description}</div>
 </section>
 
 <style lang="scss">
   section {
     display: flex;
+    flex-direction: row;
+    padding: 1%;
+    width: 100%;
+    align-items: center;
+
+    font-family: Lato;
+    
+    #name {
+      flex: 0 0 22.5%;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      font-weight: 900;
+      font-size: 1.1rem;
+      color: rgb(34, 140, 233);
+    }
+
+    #deadline {
+      flex: 0 0 7%;
+    }
+
+    #description {
+      flex: 0 0 73%;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
   }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,32 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+  import favicon from "$lib/assets/favicon.svg"
 
-	let { children } = $props();
+  let { children } = $props()
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+    rel="stylesheet"
+  />
 </svelte:head>
 
 <div class="header">
-	<div class="content">
-    	<h1 class="info">C3 App / List of summer programs & volunteering opportunities</h1>
-		<div class="routes">
-			<a class="route-link" href="./scholarships">Scholarships</a>
-			<a class="route-link" href="./internships">Internships</a>
-		</div>
-	</div>
+  <div class="content">
+    <h1 class="info">
+      C3 App / List of summer programs & volunteering opportunities
+    </h1>
+    <div class="routes">
+      <a class="route-link" href="./scholarships">Scholarships</a>
+      <a class="route-link" href="./internships">Internships</a>
+    </div>
+  </div>
 </div>
 
 {@render children()}
 
-
-
 <style lang="scss">
-
 </style>
