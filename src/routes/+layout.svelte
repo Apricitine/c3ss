6cca6dc0b7dc74391a00fea98c702dd1642d8387
apricitine <apricitinecas@gmail.com>
2026-03-04 13:04:42 -0800

    asdf

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,84 +1,33 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+  import favicon from "$lib/assets/favicon.svg"
 
-	let { children } = $props();
+  let { children } = $props()
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <link rel="icon" href={favicon} />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+    rel="stylesheet"
+  />
 </svelte:head>
 
 <div class="header">
-	<div class="content">
-    	<h1 class="info">C3 App / List of summer programs & volunteering opportunities</h1>
-	</div>
-
-	<div class="routes">
-		<div class="r"><a >Home</a></div>
-		<div class="r"><a class="route-link" href="./scholarships">Scholarships</a></div>
-		<div class="r"><a class="route-link" href="./internships">Internships</a></div>
-	</div>
+  <div class="content">
+    <h1 class="info">
+      C3 App / List of summer programs & volunteering opportunities
+    </h1>
+    <div class="routes">
+      <a class="route-link" href="./scholarships">Scholarships</a>
+      <a class="route-link" href="./internships">Internships</a>
+    </div>
+  </div>
 </div>
 
 {@render children()}
 
-
-
 <style lang="scss">
-	@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
-	
-	h1.info, a {
-		text-align: center;
-		font-family: "Roboto", sans-serif;
-		font-weight: 700;
-	}
 
-	h1 {
-		color: white;
-
-		text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
-	}
-
-	a {
-		color: white;
-		font-size: 25px;
-		text-decoration: none;
-
-		text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
-	}
-
-	a:hover {
-		color: #F0F0F0;
-	}
-
-	div.content {
-		background: linear-gradient(135deg, #E33434 0%, #F2AC2C 100%);
-		width: auto;
-		height: 300px;
-		padding: 0 0 0 0;
-		margin: 0 0 0 0;
-
-		display: grid;
-		place-items: center;
-		height: 200px;
-	}
-
-	div.routes {
-		width: 100%;
-		display: flex;
-  		justify-content: space-around;
-	}
-
-	div.r {
-		width: 100%;
-		border: 1px solid black;
-		text-align: center;
-		background-color: #DBDBDB;
-		padding: 20px;
-		
-	}
-
-	div.end {
-		border: 1px solid black;
-	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,22 @@
-<script>
-  let { data } = $props();
+<script lang="ts">
+  import Scholarship from "$lib/components/Scholarship.svelte"
+
+  let { data } = $props()
+
+  let formatDate = (date: Date) => `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+  let noZero = (str: string): string => str.replace(/^0+(?!$)/, "")
+
 </script>
 
 <div class="border"></div>
 
-
-
 <div class="end"></div>
 
-<ul>
-{#each data.scholarships as scholarship}
-    <li><a href={scholarship.primary_link}>{scholarship.name}</a> - {scholarship.description}</li>
-{/each}
-</ul>
+
+  {#each data.scholarships as scholarship}
+    <Scholarship name={scholarship.name} deadline={formatDate(new Date(scholarship.deadline))} description={scholarship.description}/>
+  {/each}
+
 
 <style lang="scss">
-
 </style>
