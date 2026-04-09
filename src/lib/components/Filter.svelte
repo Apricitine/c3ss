bf0de35f4dfac6dc8a503cc1fff810ffb8372c0b
kenny lee <kenjlee09@gmail.com>
2026-04-09 12:50:10 -0700

    filter dropdown

---
+++ src/lib/components/Filter.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    let isDroppedDown = false
+    const handleDropDownClick = () => {
+        isDroppedDown = !isDroppedDown //toggle dropdown
+    }
+
+    const handleDropdownFocusLoss = (event: FocusEvent): void => {
+        const { relatedTarget, currentTarget } = event;
+        if (
+            relatedTarget instanceof HTMLElement &&
+            currentTarget instanceof HTMLElement &&
+            currentTarget.contains(relatedTarget)
+        ) { return }
+        isDroppedDown = false;
+    };
+
+</script>
+
