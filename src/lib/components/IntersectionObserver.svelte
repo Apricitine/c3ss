4a663971d1005f2bbf78f6abce907ea643884425
kenny lee <kenjlee09@gmail.com>
2026-04-10 20:31:22 -0700

    Implementing IntersectionObserver

---
+++ src/lib/components/IntersectionObserver.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    const options: IntersectionObserverInit = {
+        root: document.querySelector("#scrollArea"),
+        rootMargin: "0px",
+        threshold: 1.0,
+    }
+    let container
+    export let once = false
+    let intersecting = false
+    const observer = new IntersectionObserver(entries => {
+        intersecting = entries[0].isIntersecting
+        if (intersecting && once) {
+            observer.unobserve(container)
+        }
+    })
+
+    observer.observe(continer)
+    return () => observer.unobserve(container)
+
+    function handler() {
+        const bcr = container.getBoundingClientRect()
+        intersecting = 
+    }
+
+
+</script>
