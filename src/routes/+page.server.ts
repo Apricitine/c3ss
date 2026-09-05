42ba2f31144499215ba2c76040c55602d7a286b0
Apricitine <apricitinecas@gmail.com>
2026-09-05 15:29:47 -0700

    its gone

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
-// import { redirect } from '@sveltejs/kit'
-//
-// export function load() {
-//   redirect(308, '/scholarships')
-// }
+import { redirect } from '@sveltejs/kit'
+
+export function load() {
+  redirect(308, '/scholarships')
+}
